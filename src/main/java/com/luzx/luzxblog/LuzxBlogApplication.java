34206f7525e85a230b3a3548a258795288c470bd
luzhenxian <lzxsir@foxmail.com>
2024-08-11 22:01:18 +0800

    first Commit

---
+++ src/main/java/com/luzx/luzxblog/LuzxBlogApplication.java
@@ -0,0 +1,13 @@
+package com.luzx.luzxblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuzxBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuzxBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luzx/luzxblog/LuzxBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luzx.luzxblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuzxBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
